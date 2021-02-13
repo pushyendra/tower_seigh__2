0763113a9ef0b2b589a8f07b7f6713cf948792ee
pushyendra <74911783+pushyendra@users.noreply.github.com>
2021-02-13 21:18:12 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box{
+  constructor(x, y, width, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y,width, height, options);
+      this.width = width;
+      this.height = height;
+
+      // this.image = loadImage("sprites/base.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      fill(118, 89, 222)
+      // imageMode(CENTER);
+      // image(this.image, 0, 0, this.width, this.height);
+      rect(0,0,this.width,this.height)
+      pop();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tower_seigh__2
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
